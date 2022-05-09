0fcf4f976b7822c223844b580f634417e80a3825
Syed Muhammad Bin Asif <92625830+syedmba@users.noreply.github.com>
2022-05-09 16:13:33 +0500

    Create game.cpp

---
+++ game.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "game.h"
+
+using namespace std;
+
+struct Snake{
+    int length;
+    int speed;
+    int *positionArray;
+};
+
+// void updatePositionArray() ???
+
+bool Game(){
+    
+}
