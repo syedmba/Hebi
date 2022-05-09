9d1516536d506b7d5c1308a7e2117749d792f4bb
Syed Muhammad Bin Asif <92625830+syedmba@users.noreply.github.com>
2022-05-09 22:17:07 +0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,35 +1,87 @@
 #include <iostream>
 #include <string>
+#include <ncurses.h>
+#include <cmath>
+#include <iomanip>
+#include <unistd.h>
 
 #include "game.h"
 
+#define BLOCK   "\xE2\x96\x88"
+
 using namespace std;
 
-struct Snake{
-    int length;
-    int speed;              // will not implement right now
-    int **positionArray;
-};
+
 
 // void updatePositionArray() ???
 
-string determineDirection(Snake snake){
+void printFrame(Snake snake){
+    initscr();
+    for (int i = 0; i < areaHeight; i++){
+        for (int j = 0; j < areaLength; j++){
+            mvprintw(j, i, " ");
+            if (i == areaHeight - 1 || i == 0 || j == 0 || j == areaLength - 1){
+                mvprintw(j, i, BLOCK);
+            }
+        }
+    }
+    for (int i = 0; i < snake.length; i++){
+        mvprintw(snake.positionArray[i][0], snake.positionArray[i][1], BLOCK);
+    }
+}
 
+string determineDirection(Snake snake){
+    int xhead = snake.positionArray[0][0];
+    int yhead = snake.positionArray[0][1];
+    int xsecond = snake.positionArray[1][0];
+    int ysecond = snake.positionArray[1][1];
+    if (xhead == xsecond + 1){
+        return "RIGHT";
+    } else if (xhead == xsecond - 1){
+        return "LEFT";
+    } else if (yhead == ysecond + 1){
+        return "UP";
+    } else {
+        return "DOWN";
+    }
 }
 
 void generateNextFrame(Snake snake ,string playerInput){
+    string direction = determineDirection(snake);
     if (playerInput == "UP"){
-
+        if (direction != "DOWN"){
+            for (int i = snake.length - 1; i > 0; i--){
+                snake.positionArray[i][0] = snake.positionArray[i-1][0];
+                snake.positionArray[i][1] = snake.positionArray[i-1][1];
+            }
+            snake.positionArray[0][1] = snake.positionArray[0][1] + 1;
+        }
     } else if (playerInput == "DOWN"){
-
+        if (direction != "UP"){
+            for (int i = snake.length - 1; i > 0; i--){
+                snake.positionArray[i][0] = snake.positionArray[i-1][0];
+                snake.positionArray[i][1] = snake.positionArray[i-1][1];
+            }
+            snake.positionArray[0][1] = snake.positionArray[0][1] - 1;
+        }
     } else if (playerInput == "LEFT"){
-        
+        if (direction != "RIGHT"){
+            for (int i = snake.length - 1; i > 0; i--){
+                snake.positionArray[i][0] = snake.positionArray[i-1][0];
+                snake.positionArray[i][1] = snake.positionArray[i-1][1];
+            }
+            snake.positionArray[0][0] = snake.positionArray[0][0] - 1;
+        }
     } else if (playerInput == "RIGHT"){
-        if (snake.positionArray[1][0] != snake.positionArray[0][0] + 1){    // if second part of body is not to right of head
-            
+        if (direction != "LEFT"){
+            for (int i = snake.length - 1; i > 0; i--){
+                snake.positionArray[i][0] = snake.positionArray[i-1][0];
+                snake.positionArray[i][1] = snake.positionArray[i-1][1];
+            }
+            snake.positionArray[0][0] = snake.positionArray[0][0] + 1;
         }
     } else {
-        string direction = determineDirection(snake);
+        generateNextFrame(snake, direction);
     }
 }
 
@@ -38,9 +90,37 @@ bool Game(){
     snake.length = 2;
     snake.speed = 1;        // will not implement for now
 
+
+    int area[areaHeight][areaLength];
+
     snake.positionArray = new int*[snake.length];
     for (int i = 0; i < snake.length; i++){
         snake.positionArray[i] = new int[2];
     }
+
+    bool Alive = true;
+    string playerInput;
+
+    while (Alive){
+        playerInput = takeInput();
+        generateNextFrame(snake, playerInput);
+        printFrame(snake);
+    }
+
+    return 1;
     
+}
+
+string takeInput(){
+    string response;
+    response = getch();
+    if (response != "^["){
+        takeInput();
+    }
+    getch();
+    response = getch();
+    if (response == "A" || response == "B" || response == "C" || response == "D"){
+        return response;
+    }
+    return takeInput();
 }
