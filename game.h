1d1dc6bfc13d02e49a02957fd26e3866798772bf
Syed Muhammad Bin Asif <92625830+syedmba@users.noreply.github.com>
2022-05-09 22:17:09 +0500

    Update game.h

--- game.h
+++ game.h
@@ -3,7 +3,22 @@
 #ifndef GAME
 #define GAME
 
+#include <ncurses.h>
+
+struct Snake{
+    int length;
+    int speed;              // will not implement right now
+    int **positionArray;
+};
+
+const int areaHeight = 30;
+const int areaLength = 50;
+
 bool Game();
+std::string determineDirection(Snake snake);
+void generateNextFrame(Snake snake ,std::string playerInput);
+std::string takeInput();
+void printFrame(Snake snake);
 
 #endif
 
