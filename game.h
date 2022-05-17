3e11a2a05b22114fc39180439baa6acd21dc82f9
Syed Muhammad Bin Asif <92625830+syedmba@users.noreply.github.com>
2022-05-17 16:12:04 +0500

    Update game.h

--- game.h
+++ game.h
@@ -4,14 +4,21 @@
 #define GAME
 
 #include <ncurses.h>
+#include <vector>
 
 #define BLOCK   "\xE2\x96\x88"
 // "\u2588"
 
+struct Coords{
+    int x;
+    int y;
+};
+
 struct Snake{
     int length;
     int speed;              // will not implement right now
-    int **positionArray;
+    // int **positionArray;
+    std::vector<Coords> positionArray;
 };
 
 const int areaHeight = 30;
@@ -19,10 +26,11 @@ const int areaLength = 50;
 
 bool Game();
 std::string determineDirection(Snake snake);
-void generateNextFrame(Snake snake ,std::string playerInput);
+void generateNextFrame(Snake &snake ,std::string playerInput);
 std::string takeInput();
 void printFrame(Snake snake);
 void generateCollectibles(Snake snake, int collectibleAt[2]);
+void makeSnakeLonger(Snake &snake, int collectibleAt[2]);
 
 #endif
 
